3af6a53289d9d09c1418b6bcfab4104d71e5d157
Jakhongir <jakhongir.nishanbaev@gmail.com>
2022-08-12 08:53:53 +0500

    feat: add sorting logic

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+    if (matrix === undefined) {
+        return [];
+    }
+
+    return matrix
+        .map((array, index) => {
+            if (index%2 !== 0) {
+                array.sort((a, b) => b - a);
+            }
+
+            return array;
+        })
+        .reduce(function (previousValue, array) {
+            previousValue.push(...array);
+
+            return previousValue;
+        }, []);
 }
